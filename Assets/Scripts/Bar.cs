d19532a3416fe62329d35d7695809602a445a2db
NicovPelt <nicobn@live.nl>
2016-09-06 12:40:35 +0300

    First project commit

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Bar : MonoBehaviour, IDropHandler {
+	
+	#region IDropHandler implementation
+	void IDropHandler.OnDrop (PointerEventData eventData)
+	{
+		eventData.pointerDrag.transform.SetParent (transform);
+		arrangeBlocks ();
+	}
+	#endregion
+
+	public void arrangeBlocks(){
+		for (int i = 0; i < transform.childCount; i++) {
+			Vector2 temp = new Vector2 ((18 + (i * 34)), 17);
+			transform.GetChild (i).gameObject.GetComponent<RectTransform> ().anchoredPosition = temp;
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	private RectTransform rTransform;
+
+	#region IDragHandler implementation
+	void IDragHandler.OnDrag (PointerEventData eventData)
+	{
+		
+		//Vector2 pos;// = eventData.position;
+		//RectTransformUtility.ScreenPointToLocalPointInRectangle(GameObject.FindGameObjectWithTag("Canvas").transform as RectTransform, Input.mousePosition, GameObject.FindGameObjectWithTag("Canvas").GetComponent<Canvas>().worldCamera, out pos);
+		rTransform.anchoredPosition = Input.mousePosition;
+	}
+	#endregion
+
+	#region IBeginDragHandler implementation
+
+	void IBeginDragHandler.OnBeginDrag (PointerEventData eventData)
+	{
+		var temp = transform.parent.gameObject.GetComponent<Bar> ();
+		transform.SetParent(transform.parent.parent);
+		temp.arrangeBlocks ();
+		GetComponent<Image> ().raycastTarget = false;
+	}
+
+	#endregion
+
+	#region IEndDragHandler implementation
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		GetComponent<Image> ().raycastTarget = true;
+	}
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		rTransform = gameObject.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
