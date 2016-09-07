d1f969fbeb1a05c19a86dda8ce084c91c1cd7d68
jylenika <jylenika@gmail.com>
2016-09-07 10:14:45 +0300

    Question Screen is activated after keyboard button I is pressed

---
+++ Assets/Scripts/QB_call.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class QB_call : MonoBehaviour {
+	public GameObject QuestionScreen;
+
+		void Update () {
+				
+		if (Input.GetKeyDown (KeyCode.I)) {
+			QuestionScreen.SetActive (true);
+		}
+	
+	}
+}
